1306957610750e186edec479f83474c387d846af
chaim1 <cohen1ch@gmail.com>
2018-12-24 18:04:42 +0200

    update

--- app/public/js/media.js
+++ app/public/js/media.js
@@ -1,20 +1,70 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
-console.log(width, height);
+// console.log(width, height);
+var a =$( '.main' ).height();
+console.log(a);
 
 // for mobile
+$('.hide').hide();
+$('.hide_menu').hide();
+$('.borderNav').hide();
+
+
 window.addEventListener('scroll', function (e) {
-    // e.preventDefault();
-    const lastPosition = window.scrollY
+    e.preventDefault();
+    let lastPosition = window.scrollY;
 
     if(lastPosition > 50){
         $('.heading-primary').fadeOut();
         $('.header__logo').fadeOut();
+        $('.borderNav').fadeIn();
+        if(lastPosition<height){
+            $('.hide').fadeIn();
+        }
     }else if (lastPosition < 50){
         $('.heading-primary').fadeIn();
         $('.header__logo').fadeIn();
+        $('.hide').fadeOut();
+        $('.borderNav').fadeOut();
+
     }
     console.log(lastPosition);
 });
 
 
+
+// for both
+
+
+
+window.addEventListener('scroll', function (e) {
+    let lastPosition = window.scrollY
+    if(lastPosition>height+3.5){
+
+        if (width <= 600) {  
+            document.getElementById('main').style.backgroundAttachment = 'fixed';
+            $('.hide_menu').fadeIn();
+            $('.hide').hide();
+        }
+
+    }else if(lastPosition < height){
+        if (width <= 600) {  
+            document.getElementById('main').style.backgroundAttachment = 'scroll';
+            $('.hide_menu').fadeOut();
+        }
+    }
+
+});
+
+
+
+// $('#main').css(backgroundAttachment,'fixed');
+// if(lastPosition>height){
+//     console.log(lastPosition,height);
+//     document.getElementById('space').style.zIndex = '1';
+//     document.getElementById('main').style.backgroundAttachment = 'scroll';
+//     $('.main').css('position:','fixed');
+// $( document ).ready(function() {
+//  var a =$( '.main' ).height();
+// console.log(a);
+// });
--- index.php
+++ index.php
@@ -7,13 +7,24 @@
     <link rel="stylesheet" href="app/public/css/style.css">
     <link rel="stylesheet" href="app/public/css/media.css">
     <link href="https://fonts.googleapis.com/css?family=Charm|Coustard" rel="stylesheet">
-    <link rel='stylesheet' href='node_modules\bootstrap\dist\css\bootstrap.min.css'>
-    <script src='node_modules\jquery\dist\jquery.js'></script>
-    <title>la-piedra</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">    <script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>    <title>la-piedra</title>
 </head>
 <body>
-    <header id="header">
-        <p class="fixed">jdscvjs</p>
+    <header class="" id="header">
+        <div class="borderNav">
+            <div class="navLines"></div>
+            <div class="navLines"></div>
+            <div class="navLines"></div>
+        </div>
+        <div class="header__logo-box__scroll hide">
+            <img src="app/public/img/imageedit_2_3836448229.png" alt="logo" class="header__logo__scroll" >
+        </div>
+        <div class="header__logo-box__scroll hide_menu">
+            <img src="app/public/img/imageedit_2_4881948661.png" alt="logo" class="header__logo__scroll" >
+        </div>
         <div class="header__logo-box">
             <img src="app/public/img/imageedit_2_3836448229.png" alt="logo" class="header__logo" >
         </div>
@@ -25,9 +36,122 @@
             </h1>
         </div>
     </header>
-    <main class="main">
+    <main class="whiteColor main" id="main">
+        <h1 class="textCenter header__title">פיצות</h1>
+        <hr class="LineHeaderTitle">
 
+        <h2 class="textCenter sub__title">פיצות אדומות</h2>
+        <hr class="LineHeaderSubTitle">
+        <div class="ItemContent whiteColor">
+            <h3 class="textCenter  headerContentMenu">נפוליטנה</h3>
+            <p class="ContentMenu">
+                מוצרלה, 
+            </p>
+            <p class="ContentMenu">
+                 עגבניות,
+            </p>
+            <p class="ContentMenu">
+                בזיליקום
+            </p>
+            <p class="ContentMenu">
+                מוצרלה, 
+            </p>
+            <p class="ContentMenu">
+                 עגבניות,
+            </p>
+            <p class="ContentMenu">
+                בזיליקום
+            </p>
+            <p class="ContentMenu">
+                מוצרלה, 
+            </p>
+            <p class="ContentMenu">
+                 עגבניות,
+            </p>
+            <p class="ContentMenu">
+                בזיליקום
+            </p>
+            <!-- <div class="clearFloat"></div> -->
+            <strong class="price">מחיר:44שח</strong>    
+        </div>
+        <div class="ItemContent whiteColor">
+            <h3 class="textCenter  headerContentMenu">נפוליטנה</h3>
+            <p class="ContentMenu">
+                מוצרלה, 
+            </p>
+            <p class="ContentMenu">
+                 עגבניות,
+            </p>
+            <p class="ContentMenu">
+                בזיליקום
+            </p>
+            <p class="ContentMenu">
+                מוצרלה, 
+            </p>
+            <p class="ContentMenu">
+                 עגבניות,
+            </p>
+            <p class="ContentMenu">
+                בזיליקום
+            </p>
+            <p class="ContentMenu">
+                מוצרלה, 
+            </p>
+            <p class="ContentMenu">
+                 עגבניות,
+            </p>
+            <p class="ContentMenu">
+                בזיליקום
+            </p>
+            <!-- <div class="clearFloat"></div> -->
+            <strong class="price">מחיר:44שח</strong>    
+        </div>
+
+        <h1 class="textCenter header__title">פיצות</h1>
+        <hr class="LineHeaderTitle">
+
+        <h2 class="textCenter sub__title">פיצות אדומות</h2>
+        <hr class="LineHeaderSubTitle">
+        <div class="ItemContent whiteColor">
+            <h3 class="textCenter  headerContentMenu">נפוליטנה</h3>
+            <p class="ContentMenu">
+                מוצרלה, 
+            </p>
+            <p class="ContentMenu">
+                 עגבניות,
+            </p>
+            <p class="ContentMenu">
+                בזיליקום
+            </p>
+            <p class="ContentMenu">
+                מוצרלה, 
+            </p>
+            <p class="ContentMenu">
+                 עגבניות,
+            </p>
+            <p class="ContentMenu">
+                בזיליקום
+            </p>
+            <p class="ContentMenu">
+                מוצרלה, 
+            </p>
+            <p class="ContentMenu">
+                 עגבניות,
+            </p>
+            <p class="ContentMenu">
+                בזיליקום
+            </p>
+            <!-- <div class="clearFloat"></div> -->
+            <strong class="price">מחיר:44שח</strong>    
+        </div>
+        
     </main>
+
+
+    <section class="about">
+        <h1 class="textCenter whiteColor">אודות</h1>
+        <p class="aboutContant">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque dolorum, sunt et ratione dolorem, ducimus explicabo neque tempora optio magni sequi possimus voluptatum unde nihil enim suscipit minima nulla. Tempora.</p>
+    </section>
 
 
 
